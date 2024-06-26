19522a115d62ee4924e4ae0ce0c22dd501c6cf33
janitochvvll2311 <janitochvvll2311@gmail.com>
2024-06-26 12:15:52 -0600

    Locale script updated

--- JS/Locale.js
+++ JS/Locale.js
@@ -1,22 +1,22 @@
-if (!localStorage.locale)
-    localStorage.locale = navigator.language.split("-")[0];
+const translations = {};
+
+async function getTranslation(locale = null) {
+    locale ??= localStorage.locale;
+    let translation = translations[locale];
+    if (translation) return translation;
+    let path = location.pathname.replace(".html", "");
+    let source = `/Locales${path == "/" ? "/Index" : path}.${locale}.json`;
+    let response = await fetch(source);
+    translation = await response.json();
+    translation[locale] = translation;
+    return translation
+}
 
 async function setLocale(locale = null) {
     locale ??= localStorage.locale;
     localStorage.locale = locale;
 
-    let source;
-    let currentPath = location.pathname;
-    if(currentPath === "/"){
-        source = `/Locales/index.${locale}.json`;
-    }
-    else{
-        source = `/Locales${location.pathname.split(".")[0]}.${locale}.json`;
-    }
-
-    
-    let response = await fetch(source);
-    let json = await response.json();
+    let translation = await getTranslation(locale);
 
     let elements = document.querySelectorAll("[data-locale]");
     for (let element of elements) {
@@ -29,22 +29,26 @@ async function setLocale(locale = null) {
                     obj[entry[0]] = entry[1];
                     return obj;
                 }, {});
-            if (options.text && json[options.text])
-                element.innerText = json[options.text];
-            if (options.html && json[options.html])
-                element.innerHTML = json[options.html];
-            if (options.value && json[options.value])
-                element.value = json[options.value];
-            if (options.placeholder && json[options.placeholder])
-                element.placeholder = json[options.placeholder];
-            if (options.title && json[options.title])
-                element.title = json[options.title];
+            if (options.text && translation[options.text])
+                element.innerText = translation[options.text];
+            if (options.html && translation[options.html])
+                element.innerHTML = translation[options.html];
+            if (options.value && translation[options.value])
+                element.value = translation[options.value];
+            if (options.placeholder && translation[options.placeholder])
+                element.placeholder = translation[options.placeholder];
+            if (options.title && translation[options.title])
+                element.title = translation[options.title];
         }
     }
 }
 
-setLocale();
-
 export {
+    getTranslation,
     setLocale
 }
+
+if (!localStorage.locale)
+    localStorage.locale = navigator.language.split("-")[0];
+
+setLocale();
